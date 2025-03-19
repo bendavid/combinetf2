0983d214e156aaf9baaef5c6bf773a0e63cfc5c4
Josh Bendavid <Josh.Bendavid@cern.ch>
2025-03-19 09:03:55 -0400

    compute and store estimated distance to minimum

--- bin/combinetf2_fit.py
+++ bin/combinetf2_fit.py
@@ -368,6 +368,8 @@ def save_hists(args, fitter, ws, prefit=True):
 
 def fit(args, fitter, ws, dofit=True):
 
+    edmval = None
+
     if args.externalPostfit is not None:
         # load results from external fit and set postfit value and covariance elements for common parameters
         with h5py.File(args.externalPostfit, "r") as fext:
@@ -404,8 +406,22 @@ def fit(args, fitter, ws, dofit=True):
         if dofit:
             fitter.minimize()
 
+        # compute the covariance matrix and estimated distance to minimum
+
         val, grad, hess = fitter.loss_val_grad_hess()
-        fitter.cov.assign(tf.linalg.inv(hess))
+
+
+        # use a Cholesky decomposition to easily detect the non-positive-definite case
+        chol = tf.linalg.cholesky(hess)
+        del hess
+
+        gradv = grad[..., None]
+        edmval = 0.5 * tf.linalg.matmul(gradv, tf.linalg.cholesky_solve(chol, gradv), transpose_a=True)
+        print(edmval)
+
+        fitter.cov.assign(tf.linalg.cholesky_solve(chol, tf.eye(chol.shape[0], dtype=chol.dtype)))
+
+        del chol
 
         if args.doImpacts:
             ws.add_impacts_hists(*fitter.impacts_parms(hess))
@@ -422,6 +438,7 @@ def fit(args, fitter, ws, dofit=True):
     ws.results.update(
         {
             "nllvalfull": nllvalfull,
+            "edmval": edmval,
             "satnllvalfull": satnllvalfull,
             "ndfsat": ndfsat,
             "postfit_profile": fitter.profile,
